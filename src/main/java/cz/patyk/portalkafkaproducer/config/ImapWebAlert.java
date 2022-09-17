0b79771bd2e568110bc7509412118f941535638e
Martin Patyk <martin.patyk@telia.no>
2022-09-17 22:00:22 +0200

    created ImapWebAlert

---
+++ src/main/java/cz/patyk/portalkafkaproducer/config/ImapWebAlert.java
@@ -0,0 +1,15 @@
+package cz.patyk.portalkafkaproducer.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties("imap.web-alert")
+public class ImapWebAlert {
+    private String username;
+    private String host;
+    private Integer port;
+    private String password;
+}
