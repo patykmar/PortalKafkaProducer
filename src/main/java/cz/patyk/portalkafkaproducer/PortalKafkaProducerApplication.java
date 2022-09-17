ca8d4ceb7568d3a32bf6c2f49928d85fc0d44818
Martin Patyk <martin.patyk@telia.no>
2022-09-17 21:50:33 +0200

    created project

---
+++ src/main/java/cz/patyk/portalkafkaproducer/PortalKafkaProducerApplication.java
@@ -0,0 +1,13 @@
+package cz.patyk.portalkafkaproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalKafkaProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortalKafkaProducerApplication.class, args);
+    }
+
+}
